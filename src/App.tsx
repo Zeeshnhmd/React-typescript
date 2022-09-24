be56dba5e9c4a322816430228ce3e2698ef4f29b
Zeeshnh <zeeshan.ahmad@sparkeighteen.com>
2022-09-24 20:34:06 +0530

    typescript environment setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,7 @@
-import './App.css';
-import { Greet } from './components/Greet';
-import { Person } from './components/Person';
-import { PersonList } from './components/PersonList';
-import { Heading } from './components/tut5/Heading';
-import { Oscar } from './components/tut5/Oscar';
-import { Status } from './components/tut5/Status';
+import "./App.css";
 
 function App() {
-	const personName = {
-		first: 'Zeeshan',
-		last: 'Ahmad',
-	};
-
-	const personList = [
-		{
-			first: 'Zeeshan',
-			last: 'Ahmad',
-		},
-		{
-			first: 'Faizan',
-			last: 'Ahmad',
-		},
-		{
-			first: 'Jhon',
-			last: 'Doe',
-		},
-	];
-	return (
-		<div className="App">
-			<Greet name="Zeeshan" messageCount={30} isLoggedIn={true} />
-			<Greet name="Zeeshan" isLoggedIn={true} />
-			<Person name={personName} />
-			<PersonList names={personList} />
-			{/* Here in the "Status " component there is problem and it is that we can pass any thing as string and will not show us the error but what we do is that "Continue to Status component"  */}
-			<Status status="success" />
-			<Heading>Placeholder text</Heading>
-			<Oscar>
-				<Heading>Oscar goes to Leonardo Dicpario!</Heading>
-			</Oscar>
-		</div>
-	);
+	return <div className="App">hi there</div>;
 }
 
 export default App;
--- src/components/tut5/Oscar.tsx
+++
@@ -1,9 +0,0 @@
-// passing a Component as "Props" we need to use "React.ReactNode" (this is provided by React typescript)
-
-type OscarProps = {
-	children: React.ReactNode;
-};
-
-export const Oscar = (props: OscarProps) => {
-	return <div>{props.children}</div>;
-};
